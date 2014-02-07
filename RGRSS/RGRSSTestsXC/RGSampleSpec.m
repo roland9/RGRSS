20d1e84cf40834860f9f7eada6107be43d9242ae
Roland Gropmair <roland@mapps.ie>
2014-02-07 22:19:04 +0000

    added google spreadsheet test

--- RGRSS/RGRSSTestsXC/RGSampleSpec.m
+++ RGRSS/RGRSSTestsXC/RGSampleSpec.m
@@ -1,12 +1,36 @@
 #import "Kiwi.h"
+#import "RGFeedManager.h"
 
-SPEC_BEGIN(MathSpec)
 
-describe(@"Math", ^{
-    it(@"is pretty cool", ^{
-        NSUInteger a = 16;
-        NSUInteger b = 26;
-        [[theValue(a + b) should] equal:theValue(42)];
+SPEC_BEGIN(SpreadsheetTests)
+
+describe(@"Loading Spreadsheet", ^{
+    
+    beforeAll(^{
+        NSString *urlString = @"http://spreadsheets.google.com/feeds/list/0Apmsn6hlyPHudDBHbWJ6NkI4SFNwTEkzQVBXS3VGY0E/od6/public/values?alt=json";
+        NSURL *url = [NSURL URLWithString:urlString];
+        [[RGFeedManager sharedRGFeedManager] loadURL:url];
+    });
+    
+    it(@"data loaded", ^{
+        [[expectFutureValue([[RGFeedManager sharedRGFeedManager] responseEntries]) shouldEventuallyBeforeTimingOutAfter(5)] haveCountOf:41];
+        
+        [[expectFutureValue([[RGFeedManager sharedRGFeedManager] responseEntries]) shouldEventuallyBeforeTimingOutAfter(5)]
+         containObjectsInArray:@[
+                                 @{@"itemId": @"1",
+                                   @"parentId": @"0",
+                                   @"description": @"Asia"
+                                   },
+                                 @{@"itemId": @"10",
+                                   @"parentId": @"1",
+                                   @"description": @"Indonesia"
+                                   },
+                                 @{@"itemId": @"41",
+                                   @"parentId": @"6",
+                                   @"description": @"Kenya"
+                                   }
+                                 ]];
+        
     });
 });
 
